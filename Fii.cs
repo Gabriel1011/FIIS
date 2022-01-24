0f5a962e5bbd6877be07aef35f46f8c59b3eb54c
Gabriel Almeida <gabriel.rodrigues.almeida1@gmail.com>
2022-01-23 21:31:00 -0300

    Rotina para saber quanto que vai render os investimentos em FIIs

---
+++ Fii.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIIS;
+public class Fii
+{
+    public decimal QuoteValue { get; set; }
+    public decimal QuoteDiv { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+double quoteValue = 9.95;
+double quoteDiv = 0.09;
+
+double startQuote =  (int)(2000 / 10.06);
+
+double saldo = 0.0;
+
+for (int i = 0; i < 60; i++)
+{
+  startQuote += 100;
+
+  var dividendos = startQuote * quoteDiv;
+
+
+  var caixa = dividendos + saldo;
+
+  if (caixa >= quoteValue)
+  {
+    int quantidadeParaCompra = (int)(caixa / quoteValue);
+    if (quantidadeParaCompra > 1)
+      {
+        startQuote += quantidadeParaCompra;
+        saldo = Math.Round(caixa - (quantidadeParaCompra * quoteValue), 2);
+      }
+    else
+    {
+      ++startQuote;
+      saldo = caixa - quoteValue;
+    }
+  }
+  else
+    saldo = caixa;
+
+    System.Console.WriteLine($"MXRFF11: Quantidade de cotas: {startQuote} | Rendimento: R${Math.Round(dividendos, 2)} | Valor do Patrimônio: R$ {Math.Round(startQuote * quoteValue, 2)}"
+  );
+}
